0cc6ebab19356741e63b1a404de1e33db0352bf8
Mitchell Finlay <mitchell.finlay00@gmail.com>
2023-03-16 20:43:22 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    const messages = [];
+    for (const name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
